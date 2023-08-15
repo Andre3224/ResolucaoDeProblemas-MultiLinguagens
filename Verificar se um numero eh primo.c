4203d9c6ef6ba0d5109657356225baa95d01d30a
André Jr <andre3junior@gmail.com>
2023-08-15 14:39:20 -0300

    Create Verificar se um numero eh primo.c

---
+++ Verificar se um numero eh primo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <conio.h>
+/*Verificação de Números Primos: Desenvolver um algoritmo que verifica se um número inserido pelo usuário é primo ou não.*/
+
+
+void main(){
+	
+}
